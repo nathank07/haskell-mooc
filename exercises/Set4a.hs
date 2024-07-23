9492a55fe5bc0e19623b4f7b0b332484929b310a
nathank7256 <nathank7256@gmail.com>
2024-07-23 17:20:29 -0500

    Completed 6/12 Set4a

--- exercises/Set4a.hs
+++ exercises/Set4a.hs
@@ -35,7 +35,11 @@ import Data.Array
 -- you remove the Eq a => constraint from the type!
 
 allEqual :: Eq a => [a] -> Bool
-allEqual xs = todo
+allEqual [] = True
+allEqual [x] = True
+allEqual [x, y] = x == y
+allEqual [x, y, z] = x == y && y == z
+allEqual xs = head xs == last xs && (allEqual $ (init . tail) xs)
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the function distinct which returns True if all
@@ -50,7 +54,7 @@ allEqual xs = todo
 --   distinct [1,2] ==> True
 
 distinct :: Eq a => [a] -> Bool
-distinct = todo
+distinct x = length x == (length $ nub x)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the function middle that returns the middle value
@@ -63,7 +67,9 @@ distinct = todo
 --   middle 'b' 'a' 'c'  ==> 'b'
 --   middle 1 7 3        ==> 3
 
-middle = todo
+middle :: Ord a => a -> a -> a -> a
+middle x y z = (!!) (sort [x, y, z]) 1
+
 
 ------------------------------------------------------------------------------
 -- Ex 4: return the range of an input list, that is, the difference
@@ -78,8 +84,9 @@ middle = todo
 --   rangeOf [4,2,1,3]          ==> 3
 --   rangeOf [1.5,1.0,1.1,1.2]  ==> 0.5
 
-rangeOf :: [a] -> a
-rangeOf = todo
+rangeOf :: (Ord a, Num a) => [a] -> a
+rangeOf xs = last sorted - head sorted
+    where sorted = sortBy compare xs
 
 ------------------------------------------------------------------------------
 -- Ex 5: given a (non-empty) list of (non-empty) lists, return the longest
@@ -97,7 +104,19 @@ rangeOf = todo
 --   longest [[1,2,3],[4,5],[6]] ==> [1,2,3]
 --   longest ["bcd","def","ab"] ==> "bcd"
 
-longest = todo
+longest :: (Ord a) => [[a]] -> [a]
+longest [x] = x
+longest [x, y] = longest' x y
+longest (x1 : x2 : xs) 
+    | length x1 > length x2 = longest $ [x1] ++ xs
+    | length x2 > length x1 = longest $ [x2] ++ xs
+    | otherwise = longest $ longest [[x1], [x2]] ++ xs
+
+longest' :: (Ord a) => [a] -> [a] -> [a]
+longest' x y 
+    | length x > length y = x
+    | length y > length x = y
+    | otherwise = if head x < head y then x else y
 
 ------------------------------------------------------------------------------
 -- Ex 6: Implement the function incrementKey, that takes a list of
@@ -113,8 +132,8 @@ longest = todo
 --   incrementKey True [(True,1),(False,3),(True,4)] ==> [(True,2),(False,3),(True,5)]
 --   incrementKey 'a' [('a',3.4)] ==> [('a',4.4)]
 
-incrementKey :: k -> [(k,v)] -> [(k,v)]
-incrementKey = todo
+incrementKey :: (Ord k, Num v) => k -> [(k,v)] -> [(k,v)]
+incrementKey k m = map (\t -> if fst t == k then (fst t, snd t + 1) else t) m
 
 ------------------------------------------------------------------------------
 -- Ex 7: compute the average of a list of values of the Fractional
